1a2d4bd0b9a21bfe8b72110dfe2aef688a506310
kinksaiz <kinksaiz@kinksaiz-no-MacBook-Air.local>
2014-04-24 03:19:48 +0900

    test

--- hellodjango/hellodjango/settings.py
+++ hellodjango/hellodjango/settings.py
@@ -55,15 +55,15 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-		  'default': {
-			      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-			          'NAME': 'daf063vpdc9jqf',
-				      'HOST': 'ec2-54-243-49-204.compute-1.amazonaws.com',
-				          'PORT': 5432,
-					      'USER': 'hlgjcudlrhrvtc',
-					          'PASSWORD': 'bx48OfQ7NtKplah-4PNso3pckK'
-						    }
-		  }
+	'default': {
+	'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	'NAME': 'daf063vpdc9jqf',
+	'HOST': 'ec2-54-243-49-204.compute-1.amazonaws.com',
+	'PORT': 5432,
+	'USER': 'hlgjcudlrhrvtc',
+	'PASSWORD': 'bx48OfQ7NtKplah-4PNso3pckK'
+	}
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
