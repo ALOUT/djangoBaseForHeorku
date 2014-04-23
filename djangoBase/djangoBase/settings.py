cdc0244a267694d77d4ea9d661fef90d3b19399f
kinksaiz <kinksaiz@kinksaiz-no-MacBook-Air.local>
2014-04-24 08:09:36 +0900

    fdsa

--- djangoBase/djangoBase/settings.py
+++ djangoBase/djangoBase/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
@@ -26,6 +25,32 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#import os.path
+#PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
+
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+PROJECT_ROOT = here('')
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
+TEMPLATE_DIRS = (
+	 root('templates'),
+)
+
+
+#TEMPLATE_LOADERS = (
+	#'django.template.loaders.filesystem.load_template_source',
+	#'django.template.loaders.app_directories.load_template_source',
+	#   'django.template.loaders.eggs.load_template_source',
+#)
+
+#TEMPLATE_DIRS = (
+#	os.path.join(PROJECT_DIR, "/templates"),
+#	PROJECT_DIR + '/templates',
+#)
 
 # Application definition
 
--- djangoBase/djangoBase/views.py
+++ djangoBase/djangoBase/views.py
@@ -1,4 +1,21 @@
 from django.http import HttpResponse
- 
+from django.template import loader, RequestContext
+import os.path
+
 def home(request):
-	return HttpResponse("Django on Heroku!HEY HEY HEY!!")
+	
+	contexts = RequestContext(request, {
+		'title' : 'hello django!',
+		'data' : ('Hello', 'django', 'world'),
+	})
+	
+	PROJECT_DIR = os.path.dirname(__file__)
+	TESTNAME = os.path.join(PROJECT_DIR, '/templates')
+
+	here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+	PROJECT_ROOT = here('')
+	root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
+	#return HttpResponse(root('templates/home.html'))
+	template = loader.get_template('home.html')
+	return HttpResponse(template.render(contexts))
